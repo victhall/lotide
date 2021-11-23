cbbf92a06b2201c5f91809b6f8c782c329ca1f4b
“Victoria <victoriahall.dev@gmail.com>
2021-11-23 03:30:15 +0000

    Implement tail assignment

---
+++ tail.js
@@ -0,0 +1,19 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function (arr) {
+  let result = [];
+  for (let i of arr) {
+    result.push(i);
+  }
+  console.log(result.slice(1));
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
