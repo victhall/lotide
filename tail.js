af6c16a90e710743ce0dada452f21b0fb1833553
“Victoria <victoriahall.dev@gmail.com>
2021-11-26 22:21:31 +0000

    updated logic and added comments

--- tail.js
+++ tail.js
@@ -1,19 +1,29 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+const assertEqual = function(actual, expected) {
+  if(actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
   } else {
-    console.log(`🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`)
   }
-};
+  };
 
-const tail = function (arr) {
-  let result = [];
-  for (let i of arr) {
-    result.push(i);
-  }
-  console.log(result.slice(1));
-};
+const tail = function(array) {
+  //variable containing empty array to store new array
+let newArray = [];
+//looping through element of array
+for (let item of array) {
+  //adds element to the empty array
+  newArray.push(item)
+}
+//removes first element from the array
+newArray.shift()
+//prints new array to console
+console.log(newArray)
+}
 
+// Test Case: Check the original array 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+
