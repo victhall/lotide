4fc80afb2293aa36164b8b7f71065eef582ca0ce
“Victoria <victoriahall.dev@gmail.com>
2021-11-29 14:13:10 +0000

    refactored syntax using module

--- head.js
+++ head.js
@@ -1,16 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if(actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`)
-  }
-  };
+const assertEqual = require('./assertEqual');
 
   const head = function(arr) {
     //Takes first element of arr and stores it in actual variable
     actual = arr[0]
     return actual
   }
-//TEST CODE
-  assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  
+  module.exports = head;
