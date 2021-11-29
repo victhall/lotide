390ccaed54a8b1ce2af7c263b2b60899ad906c93
“Victoria <victoriahall.dev@gmail.com>
2021-11-29 14:22:06 +0000

    refactored using module

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if(actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`)
-  }
-  };
-
 const tail = function(array) {
   //variable containing empty array to store new array
 let newArray = [];
@@ -18,12 +10,8 @@ for (let item of array) {
 newArray.shift()
 //prints new array to console
 console.log(newArray)
-}
+};
 
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
+module.exports = tail;
 
 
