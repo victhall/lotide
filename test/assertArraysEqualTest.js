9dbe0b3fd1078c6f9de9e2f21183d300459ceb06
“Victoria <victoriahall.dev@gmail.com>
2021-11-29 20:10:43 +0000

    tests for assertArraysEqual

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const eqArrays = require('../eqArrays')
+const assertArraysEqual = require('../assertArraysEqual')
+
+
+//TEST CODE
+assertArraysEqual(["hello", "world", "Victoria"], ["hello", "world", "lighthouse"]);
+assertArraysEqual(["hello", "world", "Victoria"], ["hello", "world", "Victoria"]);
